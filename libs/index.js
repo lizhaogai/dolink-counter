462810982d40682b67e808dce545360ab40bb647
yifeng <493788939lizhaogai@163.com>
2015-05-15 16:48:03 +0800

    c

--- libs/index.js
+++ libs/index.js
@@ -3,13 +3,67 @@
 var requireAll = require('require-all');
 var _ = require('lodash');
 var log = require('logs').get('dolink:counter');
+var weixin = require('weixin-api');
+weixin.token = require("./token.json").token;
 
 exports.init = function (napp) {
     var adapters = requireAll(__dirname + '/adapters');
     // other static resources
     napp.app.use(napp.app.express.static(__dirname + '/..' + '/public', {maxAge: 0}));
+
+    napp.app.get('/dc/weixin', function (req, res) {
+        if (weixin.checkSignature(req)) {
+            res.send(200, req.query.echostr);
+        } else {
+            res.send(200, 'fail');
+        }
+    });
+
+    // Start
+    napp.app.post('/dc/weixin', function (req, res) {
+        weixin.loop(req, res);
+    });
+    listen(weixin);
+
     return new Counter(napp, adapters);
 };
+
+function listen(weixin) {
+
+    weixin.textMsg(function (msg) {
+        console.log("textMsg received");
+        console.log(JSON.stringify(msg));
+        weixin.sendMsg("");
+    });
+
+// 监听图片消息
+    weixin.imageMsg(function (msg) {
+        console.log("imageMsg received");
+        console.log(JSON.stringify(msg));
+        weixin.sendMsg("");
+    });
+
+// 监听位置消息
+    weixin.locationMsg(function (msg) {
+        console.log("locationMsg received");
+        console.log(JSON.stringify(msg));
+        weixin.sendMsg("");
+    });
+
+// 监听链接消息
+    weixin.urlMsg(function (msg) {
+        console.log("urlMsg received");
+        console.log(JSON.stringify(msg));
+        weixin.sendMsg("");
+    });
+
+// 监听事件消息
+    weixin.eventMsg(function (msg) {
+        console.log("eventMsg received");
+        console.log(JSON.stringify(msg));
+        weixin.sendMsg("");
+    });
+}
 
 function Counter(napp, adapters) {
     log.debug('Start initialize...');
